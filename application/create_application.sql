8f55be4a69b66f604b869187297194e07179c165
surenyx <persianarno@gmail.com>
2022-11-19 10:29:44 +0330

    App Stable
    Commiting Latest App Stable Version

--- application/create_application.sql
+++ application/create_application.sql
@@ -58,7 +58,7 @@ wwv_flow_api.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'INVENTORY MANAGEMENT'
 ,p_last_updated_by=>'ADMIN'
-,p_last_upd_yyyymmddhh24miss=>'20221114083852'
+,p_last_upd_yyyymmddhh24miss=>'20221114085053'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>3
 ,p_ui_type_name => null
